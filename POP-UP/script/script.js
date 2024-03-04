100312cb60a55b3d5cdde56adc27b47b6313cab7
maric33 <161638899+maric33@users.noreply.github.com>
2024-03-04 17:58:40 +0100

    Added Javascript functionality to buttons and styled a little bit more

---
+++ POP-UP/script/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function toggleNewsLetter() {
+     const newsLetter = document.getElementById('newsLetter');
+     const btn = document.getElementById('id');
+
+     newsLetter.classList.toggle('active');
+    
+}
